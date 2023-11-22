c5f40ced4768718ca79fe205631e30f74771f0f9
Jesse Ramirez <gideonramirezz@gmail.com>
2023-11-21 20:46:37 -0600

    added functionality to all books & authors buttons and alows the user to delete

---
+++ .history/api/authorData_20231121202925.js
@@ -0,0 +1,47 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// FIXME:  GET ALL AUTHORS
+const getAuthors = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// FIXME: CREATE AUTHOR
+const createAuthor = () => {};
+
+// FIXME: GET SINGLE AUTHOR
+const getSingleAuthor = () => {};
+
+// FIXME: DELETE AUTHOR
+const deleteSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch`${endpoint}/author/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type'
+    }
+  }
+});
+
+// FIXME: UPDATE AUTHOR
+const updateAuthor = () => {};
+
+// TODO: GET A SINGLE AUTHOR'S BOOKS
+const getAuthorBooks = () => {};
+
+export {
+  getAuthors,
+  createAuthor,
+  getSingleAuthor,
+  deleteSingleAuthor,
+  updateAuthor,
+  getAuthorBooks,
+};
